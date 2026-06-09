1e2541bf34ae2124a9919c6ebeb91e6a2370bde9
delvin828279 <delvin828279@gmail.com>
2026-06-09 22:08:39 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-    res.send('<h1>تست موفقیت‌آمیز بود! صفحه اصلی بدون مشکل بالا آمد. 🚀</h1>');
+    res.send('<h1>اتصال مستقیم روی پورت ۸۰ برقرار شد! 🚀</h1>');
 });
 
 app.listen(port, () => {
-    console.log(`Test server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
